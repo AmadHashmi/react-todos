f5bb003b6f8161f5a584138eb904ea8b9eef440a
Amad Ul Hassan <ah@indigilabs.com>
2024-12-25 13:37:41 +0100

    created a new component for row item
    created a new component for row item

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import logo from "./logo.svg";
 import "./App.css";
+import TodoRowItem from "./components/TodoRowItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Our First application</p>
-      </header>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your todos</div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Description</th>
+                <th scope="col">Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              <TodoRowItem />
+
+              <tr>
+                <th scope="row">2</th>
+                <td>Get hair cut</td>
+                <td>Eric</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoRowItem.jsx
@@ -0,0 +1,10 @@
+function TodoRowItem() {
+  return (
+    <tr>
+      <th scrope="row">1</th>
+      <td>Feed dog</td>
+      <td>Eric</td>
+    </tr>
+  );
+}
+export default TodoRowItem;
